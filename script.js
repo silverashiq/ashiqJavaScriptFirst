31c75261e916be0f2e722ee7620376098f2a2451
silverashiq <silverashiq@gmail.com>
2024-05-04 22:53:42 +0600

    submit answers

---
+++ script.js
@@ -0,0 +1,52 @@
+//1 How to connect Js and Html? Connect Them
+// To connect .js file to .html file, we need to use script tag in the last line of Body Selection for html file, and then use src to locate the Js file -
+
+// <body>
+
+//     <script src="./jsDirectory"></script>
+// </body>
+
+
+
+
+//2 How to Write Variables in Javascript? Write at least 3 Variables
+// To declare Variables in Js, we can start with var/let/const keyword, and then a name, then = then value and end with ;
+
+// var day=14;
+// let month=5;
+// const year=1994;
+// console.log(day, month, year);
+
+
+
+
+//3 What is the difference between let, var, and const variables?
+// "var" can change value and also re-declare an existing duplicate-name (which may cause issue as the latest new name will be printed)
+// "let" on the other hand, does not allow 2 same named duplicates to exist, so once a name is declared by "let", then that name can not be re-declared again. However, we can still change it's value. 
+// const  does not allow to re-declare same name like"let", and also restricts changing it's value once assigned.
+
+
+
+
+
+//4 Suppose you want to print your name on the console. How do you write that?
+// In order to print a Text in Console section, we can write the following code-
+// console.log("Ashiqur");
+
+
+
+
+//5 Suppose you want to print your name on the alert box. How do you write that?
+// In order to print a Text in the Alert section, we can write the following code-
+// alert("Rahman");
+
+
+
+
+//6 Take- two variables, the first number, and the second number, and then print their sum on the console.
+// var firstNumber=49;
+// var secondNumber=51;
+
+// var sum= firstNumber + secondNumber;
+
+// console.log(sum);
